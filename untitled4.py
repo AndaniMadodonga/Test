fb2644cfaf45ebe7c838854cecf52a549c2d322d
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-03 22:24:18 +0200

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -385,7 +385,7 @@ def main():
     # display the front end aspect
     
     #data_load= st.file_uploader("Choose a XLSX file",type="xlsx")
-    data_load = st.file_picker("Choose a XLSX file",folder="my_folder",type="xlsx")
+    data_load = st.file_picker("Choose a XLSX file",folder="https://github.com/AndaniMadodonga/Test/blob/main/",type="xlsx")
     Label_list=labels["field_name"].values.tolist() 
     
     

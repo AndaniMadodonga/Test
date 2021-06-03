f577d338a01da717328d212c7376427d583aceb7
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-03 17:44:16 +0200

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -29,9 +29,9 @@ from deep_translator import GoogleTranslator
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import string
 #from sklearn.cluster import KMeans
-from sklearn.cluster import MiniBatchKMeans
+#from sklearn.cluster import MiniBatchKMeans
 #from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
+#from sklearn.manifold import TSNE
 import nltk
 import re
 #from sklearn.linear_model import LogisticRegression
@@ -44,7 +44,7 @@ import sklearn.metrics as metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from mlxtend.plotting import plot_confusion_matrix
+#from mlxtend.plotting import plot_confusion_matrix
 import math
 
 nltk.download('stopwords')

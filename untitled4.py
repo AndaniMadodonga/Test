71a7bdee40bf6b480ab39650647da9df864386aa
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-04 00:39:31 +0200

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -452,6 +452,7 @@ def main():
                 influence_model=influncerModel(predata)
                 inf_pred=influence_model[0].predict(influence_model[1])
                 k=pd.DataFrame(inf_pred,columns=["Influnecer_cat"])
-                st.bar_chart(k)
+                
+                st.bar_chart(k.groupby('key').count())
 if __name__ == '__main__':
     main()

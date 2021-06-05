bf235e59bd7e8e9a13c9d6a58606252864ddf64b
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-05 18:01:39 +0200

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -433,7 +433,12 @@ def main():
                                 st.write(senti[1].head())
                                 st.write("SA and Global tweet Sentiment analysis Bar graphs")
                                 #st.write(senti[0].sentiment_class.value_counts().plot(kind='bar',color='green',title="sentiment analysis for SA tweets"))
-                                st.write(senti[1].sentiment_class.value_counts().plt(kind='bar',color='red',title="sentiment analysis for Global tweets"))
+                                #st.write(senti[1].sentiment_class.value_counts().plt(kind='bar',color='red',title="sentiment analysis for Global tweets"))
+                                fig = plt.figure()
+
+                                plt.hist(senti[1].sentiment_class.value_counts(),color='red',title="sentiment analysis for Global tweets")
+
+                                st.plotly_chart(fig)
                    
                     if sent_choice=='Text':
                         keyin_text_sent = st.text_input("type or paste a tweet")

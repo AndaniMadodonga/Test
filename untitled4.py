d96fe196a592fb4225dce33442f5327a3f2d7ae8
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-05 16:23:51 +0200

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -424,7 +424,7 @@ def main():
 #                     st.bar_chart(senti[0].sentiment_class.value_counts())
 #                     st.title("sentiment analysis for International tweets")
 #                     st.bar_chart(senti[1].sentiment_class.value_counts())
-                with st.echo(code_location='below'):
+                 with st.echo(code_location='below'):
                      senti=Sent(data_model)
                      st.write("SA and Global tweet Sentiment analysis Bar graphs")
                      senti[0].sentiment_class.value_counts().plot(kind='bar',color='green',title="sentiment analysis for SA tweets")

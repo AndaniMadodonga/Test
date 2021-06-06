43cd9ab29ac5c19c72ad9ad12965a32c6b023167
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-06 22:50:17 +0200

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -395,16 +395,16 @@ def main():
                                 clean_cat=clean_cat.reset_index(drop=True)
                                 #insert pickle model
                                 
-                                
+                                st.write(clean_cat.head())
                                 pred_model=Cat_Model() 
-                                st.write(type(pred_model))
+                                
                                 categorise=pred_model.predict(clean_cat)
                                 categorise=categorise.tolist()
-                                df_class=pd.DataFrame(categorise,columns=["Class"])
+                                df_class=pd.DataFrame(categorise,columns=["Category"])
                                 df_class=df_class.reset_index(drop=True)
                                 df_cat=pd.concat([clean_cat,df_class],axis=1)
                                 st.write(df_cat.head())
-                                chart = alt.Chart(df_cat).mark_bar().encode(alt.X("Class"),y='count()').interactive()
+                                chart = alt.Chart(df_cat).mark_bar().encode(alt.X("Category"),y='count()').interactive()
                                 st.write(chart)
 
         #

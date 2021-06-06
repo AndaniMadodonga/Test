bea58875a2456a86a7e2148e5374d5d5f1c1fa04
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-06 18:50:47 +0200

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -441,7 +441,7 @@ def main():
                             senti=Sent(text.clean_text)  
                             
                             st.success('The Sentiment of the tweet is-{}'.format(senti))
-                            st.write("clean text:- {}".format(text))
+                            st.write("**clean text:-** {}".format(text.clean_text))
                             
                 
   #predata=preprocess(data)

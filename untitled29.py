98ff50b831704a84b6ff299174fffbf6656df6d1
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-08 15:04:32 +0200

    Update untitled29.py

--- untitled29.py
+++ untitled29.py
@@ -623,11 +623,14 @@ class SubSet_Data:
       import re
         
       def Find(self,string):
+          import re
           regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
           url = re.findall(regex,string)      
           return len(url)
 
       def Sentiment_url(self,corpus_df):
+          import vaderSentiment
+          from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer   
           corpus=corpus_df.to_list()
           sentences = corpus
           sentiment = list()

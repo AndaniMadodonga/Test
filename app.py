4b874e07312876c79c69f334213710bf69346ba0
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-10 20:56:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -696,7 +696,7 @@ class SubSet_Data:
                #Full_Data().main_full()
             
                 
-              data_load=Full_Data().main_Full().load()
+              data_load=Full_Data().main_full().load()
               fin_data=Full_Data().preprocess(data_load)
               st.write(fin_data=Full_Data)
               

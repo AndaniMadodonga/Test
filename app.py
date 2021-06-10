ba8de8dbae478a0ad883ef0075ba78c97d251a46
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-10 23:29:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -722,6 +722,7 @@ class SubSet_Data:
                      st.write("WordCloud")
                      data_processed_text=Full_Data().clean_text(data_processed.statuses_text)
                      st.write(data_processed_text.clean_text)
+                     st.set_option('deprecation.showPyplotGlobalUse',False)   
                      wordcloud = WordCloud(max_words=100).generate(data_processed_text.clean_text[0])
 
 # Display the generated image:

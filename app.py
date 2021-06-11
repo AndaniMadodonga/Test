73c073c214060f5192c6a210289eb0f73eb74ae6
AndaniMadodonga <70808291+AndaniMadodonga@users.noreply.github.com>
2021-06-11 12:53:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -739,7 +739,7 @@ class SubSet_Data:
           choice=st.sidebar.selectbox("Menu", pick)
           
           if choice== "Viaualization/Dashboard":
-                url="https://github.com/AndaniMadodonga/Test/blob/main/Tweetdatatest%20-%20Copy.xlsx"
+                url="https://github.com/AndaniMadodonga/Test/blob/main/Tweetdatatest%20-%20Copy.xlsx?raw=true"
                 
                 Data_file = pd.read_csv(url)
                 
